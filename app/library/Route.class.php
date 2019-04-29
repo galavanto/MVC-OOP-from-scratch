d101ead57e920032b16c2e046883dd7bce15b43e
galavanto <martijnb50@gmail.com>
2019-04-29 17:52:12 +0200

    comments at route.php

--- app/library/Route.class.php
+++ app/library/Route.class.php
@@ -12,23 +12,14 @@ class Route{
     static public function get($url,$controller){
         self::$getRoutes[$url] = $controller;
 
+        // Give web.php room for adding middleware. only accepts arrays
         return new Middleware($url);
     }
     // Static call in web.php. pushing all routes + corresponding controller into the postRoutes variable
     static public function post($url,$controller){
         self::$postRoutes[$url] = $controller;
 
+        // Give web.php room for adding middleware. only accepts arrays
         return new Middleware($url);
-    }
-
-
-    static public function middleware($middlewares){
-
-        if(is_array($middlewares)){
-            
-        }else{
-            die('Only arrays are allowed.');
-        }
-
     }
 }
